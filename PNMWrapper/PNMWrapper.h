84dafc6426166136b4442640786c2485d5f3849a
Özgün Ergen <orhanozgun.ergen@kocsistem.com.tr>
2021-08-10 13:39:09 +0300

    first commit

---
+++ PNMWrapper/PNMWrapper.h
@@ -0,0 +1,18 @@
+//
+//  PNMWrapper.h
+//  PNMWrapper
+//
+//  Created by Özgün Ergen on 10.08.2021.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for PNMWrapper.
+FOUNDATION_EXPORT double PNMWrapperVersionNumber;
+
+//! Project version string for PNMWrapper.
+FOUNDATION_EXPORT const unsigned char PNMWrapperVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <PNMWrapper/PublicHeader.h>
+
+
---
+++ PNMWrapperExample/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  PNMWrapperExample
+//
+//  Created by Özgün Ergen on 10.08.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
